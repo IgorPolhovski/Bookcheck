6b07e48f1216e3e9565bda3f3aeef5cb72ae6726
IgorPolhovski <61982046+IgorPolhovski@users.noreply.github.com>
2020-07-17 23:48:36 +0300

    Different classes from views.py united in one viewset class .gitignore added urls.py modified due to new views.py

--- stage1/booksearch/urls.py
+++ stage1/booksearch/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import BookView
-from .views import BookCreateview
-from .views import Bookbyid
+from .views import BookCreateview, BookViewSet
+
 app_name = "books"
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('books/', BookView.as_view()),
-    path('books/<int:id>/', Bookbyid.as_view()),
     path('books/create/', BookCreateview.as_view()),
+    path('books/', BookViewSet.as_view({'get': 'list'})),
+    path('books/<int:id>/', BookViewSet.as_view({'get': 'article_by_id'})),
+
 ]
--- stage1/booksearch/views.py
+++ stage1/booksearch/views.py
@@ -1,25 +1,21 @@
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
-from .serializers import BookSerializer
-from .serializers import BookbyidSerializer
-from .serializers import BookCreateSerializer
-from django.http import JsonResponse
+from .serializers import BookSerializer, BookbyidSerializer, BookCreateSerializer
 
 
-class BookView(APIView):
-    def get(self, request):
+class BookViewSet(viewsets.ViewSet):
+    def list(self, request):
         books = Book.objects.all()
         # the many param informs the serializer that it will be serializing more than a single article.
         serializer = BookSerializer(books, many=True)
         return Response({"books": serializer.data})
 
-class Bookbyid(APIView):
     def article_by_id(self, request, id):
         result = Book.objects.filter(id=id)
         serializer1 = BookbyidSerializer(result, many=True)
         return Response({"result": serializer1.data})
+
 
     def get(self, request, id):
         return self.article_by_id(self, id)
@@ -31,5 +27,20 @@ class BookCreateview(generics.CreateAPIView):
 
 
 
+# from rest_framework.views import APIView
+
+
+# class BookView(APIView):
+#     def get(self, request):
+#         books = Book.objects.all()
+#         # the many param informs the serializer that it will be serializing more than a single article.
+#         serializer = BookSerializer(books, many=True)
+#         return Response({"books": serializer.data})
+#
+# class Bookbyid(APIView):
+#     def article_by_id(self, request, id):
+#         result = Book.objects.filter(id=id)
+#         serializer1 = BookbyidSerializer(result, many=True)
+#         return Response({"result": serializer1.data})
 
 
